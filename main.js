be7de82276193f8155c477425b2efe523b29909a
Benyamin Limanto <me@benyamin.xyz>
2016-06-11 16:38:16 +0700

    CSS and JS start change Alot
    Js : It's bad that JS doesn't support tail recursion optm. so my code
    don't work well when facing a bit large of array, so I decided follow
    the old code using stack method.
    
    CSS: Change with the UI

--- main.js
+++ main.js
@@ -1,87 +1,188 @@
-/*
-					Benyamin Limanto
-	Manipulating Image, Project for Madana Web Tiles
-	Based on my Thoughts, Under survilance of my Boss :v
-					Robert William :p
-	Using Canvas for 1st time anyway, It's interesting
-	
-	Since It's using HTML Canvas, so It doesn't support cross domain image fetch,
-	so you need to change your image into a 
-*/
-
-var rpil, gpil, bpil, bayangan, domBayangan; // Warna yang user pilih untuk merubah warna itu
-var rget, gget, bget; // Warna yang select oleh user, dari bayangan
-rpil = 200; gpil = 100; bpil = 250;
 $(function(){
-	$(".tog").click(function(){
-		$(".test").toggle();
-	});
-	// CSS is sucks, using css is scalling the real width and height, so it will cause chaos
-	// http://stackoverflow.com/questions/15793702/drawing-image-on-canvas-larger-than-real
-	$("#mycanvas").attr({"width" : $(".rawdata").width(),"height" : $(".rawdata").height()});
-	$(".rawdata").ready(function() {
-		var mycanvas = $("#mycanvas").get(0); //This get same as getElementById("mycanvas"), the zero means get the first data, from jQuery
-		var ctx = mycanvas.getContext("2d");
-		var rawdata = $(".rawdata").get(0); //This get same as getElementById("mycanvas"), the zero means get the first data, from jQuery
-		ctx.drawImage(rawdata,0,0,mycanvas.width, mycanvas.height); //Like on the old times on VB, it , image resource, start x, start y and width and height
-		var imgdata = ctx.getImageData(0,0, mycanvas.width, mycanvas.height);	
-		for(i=0; i<=imgdata.data.length; i+=4){
-			var r = imgdata.data[i]; 
-			var g = imgdata.data[i+1];
-			var b = imgdata.data[i+2];
-			var a = imgdata.data[i+3];
-		}
-		ctx.putImageData(imgdata,0,0);		
-		var color = $('.hasil')[0]; var color2 = $('.hasil2')[0];
-		function pick(event) {
-			var x = event.layerX;
-			var y = event.layerY;
-			var pixel = bctx.getImageData(x, y, 1, 1);
-			var data = pixel.data;
-			var rgba = 'rgba(' + data[0] + ',' + data[1] +
-					 ',' + data[2] + ',' + data[3] + ')';
-			rget = data[0]; gget = data[1]; bget = data[2];
-			color.textContent =  rgba;
-			color.style.background = rgba;
-			var rgba = 'rgba(' + rpil + ',' + bpil +
-			',' + gpil + ',' + 255 + ')';
-			color2.textContent =  rgba;
-			color2.style.background = rgba;
-			//------------------------------
-			var mycanvas = $("#mycanvas").get(0);
-			var chasil = mycanvas.getContext("2d");
-			var imgdata = chasil.getImageData(0,0, mycanvas.width, mycanvas.height);
-			var bdata = bctx.getImageData(0,0,mycanvas.width, mycanvas.height);
-			for (var i = 0; i < imgdata.data.length; i+=4) {
-				var r = bdata.data[i];
-				var g = bdata.data[i+1];
-				var b = bdata.data[i+2];
-				var a = bdata.data[i+3];
-				if(r == rget && g == gget && b == bget){
-					imgdata.data[i] = rpil; // red
-					imgdata.data[i+1] = gpil; // green
-					imgdata.data[i+2] = bpil; // blue
-				}
-			}
-			chasil.putImageData(imgdata,0,0);
-		}
-		mycanvas.addEventListener('click',pick); // jQuery is messing with my code, the dom selector is not working properly and I don't know why, it's sucks, so I decided to rollback to Javascript		
-		
-		domBayangan = $("<canvas></canvas>").addClass("bayangan");
-		$("body").append(domBayangan);
-		$(".bayangan").css("display","none");
-		bayangan = $(".bayangan").get(0); bctx = bayangan.getContext("2d");
-		bctx.drawImage(rawdata,0,0,mycanvas.width,mycanvas.height);
-	});	
-	
-	$(".colorset .child").click(function(){
-		rpil = $(this).attr("r");
-		gpil = $(this).attr("g");
-		bpil = $(this).attr("b");
-		var color2 = $('.hasil2')[0];
-		var rgba = 'rgba(' + rpil + ',' + gpil +
-			',' + bpil + ',' + 255 + ')';
-			color2.textContent =  rgba;
-			color2.style.background = rgba;
-	});
-});	
+    var canvas = $("#that").get(0);
+    var rpil,gpil,bpil,btsR,btsG,btsG;
+    rpil = 0; gpil = 0; bpil = 0;
+    btsR = 144; btsG = 145; btsB = 131;
+    var xget, yget, cCo, iData, sr, sg, sb;
+    //Code Based on http://www.javascripter.net/faq/hextorgb.htm
+    function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+    function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+    function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+    function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+    //Code Based on http://www.javascripter.net/faq/hextorgb.htm
+    
+    $(".gambar").load(function(){
+        var gambar = $(".gambar").get(0);
+        var cContext = canvas.getContext("2d");
+        canvas.width = gambar.width;
+        canvas.height = gambar.height;
+        cContext.drawImage(gambar,0,0,gambar.width, gambar.height);
+        cCo = canvas.getContext("2d");
+    });
+    
+    $("#cpik").change(function(){
+        rpil = hexToR($(this).val());
+        gpil = hexToG($(this).val());
+        bpil = hexToB($(this).val());
+    });
+    
+    $(".clear").click(function(){
+        var gambar = $(".gambar").get(0);
+        var cContext = canvas.getContext("2d");
+        canvas.width = gambar.width;
+        canvas.height = gambar.height;
+        cContext.drawImage(gambar,0,0,gambar.width, gambar.height);
+    });
+    
+    $("#that").click(function(event){
+        var posX = event.offsetX;
+        var posY = event.offsetY;
+        getPickColor(posX,posY);
+    });
+    
+    $(".gambarpil").click(function(){
+        console.log($(this).attr("src"));
+        $(".gambar").attr("src",$(this).attr("src"));
+    });
+    
+    /*  Rekursive 2016 HTML Color Picker
+        Benyamin Limanto
+        Code works well when it's on somepart canvas that's not too big anyway
+    */
+    function bucketFloodFill(x,y){
+        posData = ((x-1)*4) + ((4*canvas.width)*(y-1));
+        iData.data[posData] = rpil;
+        iData.data[posData+1] = gpil;
+        iData.data[posData+2] = bpil;
+        //Atas
+        posData = ((x-1)*4) + ((4*canvas.width)*(y-2));
+        r = iData.data[posData]; //Red
+        g = iData.data[posData+1]; //Green
+        b = iData.data[posData+2]; //Blue
+        if ((r > btsR || g > btsG || b > btsG )&&(r != rpil || g != gpil || b != bpil)){
+            bucketFloodFill(x,y-1);
+        }
+        //Bawah
+        posData = ((x-1)*4) + ((4*canvas.width)*y);
+        r = iData.data[posData]; //Red
+        g = iData.data[posData+1]; //Green
+        b = iData.data[posData+2]; //Blue
+        if ((r > btsR || g > btsG || b > btsG )&&(r != rpil || g != gpil || b != bpil)){
+            bucketFloodFill(x,y+1);
+        }
+        //Kiri
+        posData = ((x-2)*4) + ((4*canvas.width)*(y-1));
+        r = iData.data[posData]; //Red
+        g = iData.data[posData+1]; //Green
+        b = iData.data[posData+2]; //Blue
+        if ((r > btsR || g > btsG || b > btsG )&&(r != rpil || g != gpil || b != bpil)){
+            bucketFloodFill(x-1,y);
+        }
+        //Kanan
+        posData = (x*4) + ((4*canvas.width)*(y-1));
+        r = iData.data[posData]; //Red
+        g = iData.data[posData+1]; //Green
+        b = iData.data[posData+2]; //Blue
+        if ((r > btsR || g > btsG || b > btsG )&&(r != rpil || g != gpil || b != bpil)){
+            bucketFloodFill(x+1,y);
+        }
+    }
+    /* A Code from http://www.williammalone.com/articles/html5-canvas-javascript-paint-bucket-tool/
+        Since my code works well but this's suck that JS doesn't support recursion too much because of this
+        Reffering to this.. http://programmers.stackexchange.com/questions/179863/performance-recursion-vs-iteration-in-javascript
+        JS doesn't support tail recursion optimazation
+    */
+    
+    function matchStartColor(pixelPos)
+    {
+      var r = iData.data[pixelPos];	
+      var g = iData.data[pixelPos+1];	
+      var b = iData.data[pixelPos+2];
+
+      return (r == sr && g == sg && b == sb);
+    }
+
+    function colorPixel(pixelPos)
+    {
+      iData.data[pixelPos] = rpil;
+      iData.data[pixelPos+1] = gpil;
+      iData.data[pixelPos+2] = bpil;
+    }
+    
+    function wmBucket(startX,startY){
+        pixelStack = [[startX, startY]];
+        while(pixelStack.length)
+        {
+            var newPos, x, y, pixelPos, reachLeft, reachRight;
+            newPos = pixelStack.pop();
+            x = newPos[0];
+            y = newPos[1];
+
+            pixelPos = (y*canvas.width + x) * 4;
+            while(y-- >= 0 && matchStartColor(pixelPos))
+            {
+                pixelPos -= canvas.width * 4;
+            }
+            pixelPos += canvas.width * 4;
+            ++y;
+            reachLeft = false; // Kanan
+            reachRight = false; // Kiri
+            while(y++ < canvas.height-1 && matchStartColor(pixelPos))
+            {
+                colorPixel(pixelPos);
+
+                if(x > 0)
+                {
+                    if(matchStartColor(pixelPos - 4))
+                    {
+                        if(!reachLeft){
+                            pixelStack.push([x - 1, y]);
+                            reachLeft = true;
+                        }
+                    }
+                    else if(reachLeft)
+                    {
+                        reachLeft = false;
+                    }
+                }
+
+                if(x < canvas.width-1)
+                {
+                    if(matchStartColor(pixelPos + 4))
+                    {
+                        if(!reachRight)
+                        {
+                            pixelStack.push([x + 1, y]);
+                            reachRight = true;
+                        }
+                    }
+                    else if(reachRight)
+                    {
+                        reachRight = false;
+                    }
+                }
+                pixelPos += canvas.width * 4;
+            }
+        }
+        cCo.putImageData(iData, 0, 0);
+    }
+    /*
+        end of William Malone Code. Licensed with MIT Licensed.
+        I still consider to port my recursion to iteration in meanwhile
+    */
+    function getPickColor(startX,startY){
+        iData = cCo.getImageData(startX,startY,1,1);
+        //Cek apakah hitam tidak di awal, mengurangi percabangan
+        if (iData.data[0] > btsR || iData.data[1] > btsG || iData.data[2] > btsB){
+            sr = iData.data[0]; sg = iData.data[1]; sb = iData.data[2];
+            iData = cCo.getImageData(0,0,canvas.width, canvas.height);
+            wmBucket(startX,startY);
+        }
+            /*bucketFloodFill(startX,startY);
+            cCo.putImageData(iData,0,0);
+        }*//*
+        else{
+            console.log("Gagal");
+        }*/
+    }
+});
